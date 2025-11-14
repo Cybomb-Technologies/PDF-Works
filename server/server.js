d5d1029ca4265f2d974381e2ed1f4e448585b9e8
Sudeshcybomb <sudesh.t@CybombTech.onmicrosoft.com>
2025-11-14 19:18:00 +0530

    Sudesh

--- server/server.js
+++ server/server.js
@@ -161,7 +161,7 @@ process.on("SIGINT", async () => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
